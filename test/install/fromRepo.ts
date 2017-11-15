2c0a2e7ba6d2bbc47d59dc0d6e99cb79bad66025
Zoltan Kochan <z@kochan.io>
2017-11-15 23:03:52 +0200

    feat: git dependencies can be declared with an alias name
    ref https://github.com/pnpm/pnpm/issues/838

--- test/install/fromRepo.ts
+++ test/install/fromRepo.ts
@@ -22,6 +22,38 @@ test('from a github repo', async function (t) {
   t.deepEqual(pkgJson.dependencies, {'is-negative': 'github:kevva/is-negative'}, 'has been added to dependencies in package.json')
 })
 
+test('from a github repo with different name', async function (t: tape.Test) {
+  const project = prepare(t)
+  await installPkgs(['say-hi@github:zkochan/hi#4cdebec76b7b9d1f6e219e06c42d92a6b8ea60cd'], testDefaults())
+
+  const m = project.requireModule('say-hi')
+
+  t.equal(m, 'Hi', 'dep is available')
+
+  const pkgJson = await readPkg()
+  t.deepEqual(pkgJson.dependencies, {'say-hi': 'github:zkochan/hi#4cdebec76b7b9d1f6e219e06c42d92a6b8ea60cd'}, 'has been added to dependencies in package.json')
+
+  const shr = await project.loadShrinkwrap()
+  t.deepEqual(shr.dependencies, {
+    'say-hi': 'github.com/zkochan/hi/4cdebec76b7b9d1f6e219e06c42d92a6b8ea60cd',
+  }, 'the aliased name added to shrinkwrap.yaml')
+})
+
+test('a subdependency is from a github repo with different name', async function (t: tape.Test) {
+  const project = prepare(t)
+
+  await installPkgs(['has-aliased-git-dependency'], testDefaults())
+
+  const m = project.requireModule('has-aliased-git-dependency')
+
+  t.equal(m, 'Hi', 'subdep is accessible')
+
+  const shr = await project.loadShrinkwrap()
+  t.deepEqual(shr.packages['/has-aliased-git-dependency/1.0.0'].dependencies, {
+    'say-hi': 'github.com/zkochan/hi/4cdebec76b7b9d1f6e219e06c42d92a6b8ea60cd',
+  }, 'the aliased name added to shrinkwrap.yaml')
+})
+
 test('from a git repo', async function (t) {
   if (isCI) {
     t.skip('not testing the SSH GIT access via CI')
